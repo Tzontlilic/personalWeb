e8f2ec894d97dbea7130fa8f1232955a41adef15
Tzontlilic <767406972@qq.com>
2019-10-07 22:43:16 +0800

    learn jq

--- main/src/index.js
+++ main/src/index.js
@@ -1,18 +1,35 @@
 //准备用来写轮播图
 
-var count = -1;
-function init(){
+// js写法
+/*function init(){
 	// 设置计数器
 
 	//设定计时器
-	setInterval("changeImg()", 10000);
+	setInterval("changeImg()", 5000);
 }
 
 function changeImg(){
 	count++;
-	document.getElementById("index_img").src = "../img/background"+ count +".jpg";
+
+	//document.getElementById("index_img").src = "../img/background"+ count +".jpg";
+	$("#index_img").attr("src", "../img/background" + count + ".jpg");
 	if(count == 4){
 		count = -1;	
 	}
-	
+}*/
+
+
+// jq写法
+var count = 0;
+$(function(){
+	setInterval("changeImg()", 5000);	
+});
+function changeImg(){
+
+	count++;
+	$("#index_img").attr("src", "../img/background" + count + ".jpg");  
+	if(count == 4){
+		count = -1;	
+	}
 }
+
