8d835e056c7b9b5697a6453ef178e9fe0dc31643
Tzontlilic <767406972@qq.com>
2019-10-10 20:02:07 +0800

    格式化了

--- main/src/index.js
+++ main/src/index.js
@@ -2,34 +2,34 @@
 
 // js写法
 /*function init(){
-	// 设置计数器
+    // 设置计数器
 
-	//设定计时器
-	setInterval("changeImg()", 5000);
+    //设定计时器
+    setInterval("changeImg()", 5000);
 }
 
 function changeImg(){
-	count++;
+    count++;
 
-	//document.getElementById("index_img").src = "../img/background"+ count +".jpg";
-	$("#index_img").attr("src", "../img/background" + count + ".jpg");
-	if(count == 4){
-		count = -1;	
-	}
+    //document.getElementById("index_img").src = "../img/background"+ count +".jpg";
+    $("#index_img").attr("src", "../img/background" + count + ".jpg");
+    if(count == 4){
+        count = -1;	
+    }
 }*/
 
 
 // jq写法
 var count = 0;
 $(function(){
-	setInterval("changeImg()", 5000);	
+    setInterval("changeImg()", 5000);	
 });
 function changeImg(){
 
-	count++;
-	$("#index_img").attr("src", "../img/background" + count + ".jpg");  
-	if(count == 4){
-		count = -1;	
-	}
+    count++;
+    $("#index_img").attr("src", "../img/background" + count + ".jpg");  
+    if(count == 4){
+        count = -1;	
+    }
 }
 
