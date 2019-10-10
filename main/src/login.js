0d4534ccc7fbb5d9ad4957a6e232f1c0842405cb
Tzontlilic <767406972@qq.com>
2019-10-10 14:23:23 +0800

    崩了？

--- main/src/login.js
+++ main/src/login.js
@@ -2,7 +2,7 @@
  * 用来确认表单的提交信息
  *
  */
-function check(){
+/*function check(){
 
 	var username = document.getElementById("login_username").value;
 	var password = document.getElementById("login_password").value;
@@ -20,4 +20,29 @@ function showTips(){
 	else{
 	document.getElementById("login_username_tips").innerHTML = "";
 	}
-}
+}*/
+
+// 表单校验用validate重构
+$(function(){
+	
+		
+	$("#loginFrom").validate({
+		
+		rules: {
+			login_username:{
+				required:true,
+				minlength: 6,
+				maxlength: 12
+			},		
+			
+		},
+		messages: {
+			login_username:{
+				required: "密码必填",
+				minlength: "长度不够噢",
+				maxlength: "太长了噢" 
+			},		
+			
+		}
+	});
+});
